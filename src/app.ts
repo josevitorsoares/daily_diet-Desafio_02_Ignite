10150c1383b5318df76e364b3e2e8cde22347a2d
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2023-11-10 21:53:27 -0300

    FEAT: Add middleware to handle errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from "fastify";
 import cookie from "@fastify/cookie";
+import { ZodError } from "zod";
 
 import { mealsRoutes } from "./routes/meals";
 import { usersRoutes } from "./routes/users";
+
+import { env } from "./env";
 
 export const app = fastify();
 
@@ -14,4 +17,19 @@ app.register(mealsRoutes, {
 
 app.register(usersRoutes, {
     prefix: "daily-diet/users"
+});
+
+app.setErrorHandler((error, _, reply)=> {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({
+            message: "Validation Error",
+            error: error.format()
+        });
+    }
+    if (env.NODE_ENV === "development") {
+        console.error(error);
+    } 
+    return reply.status(500).send({
+        error: "Internal Server Error"
+    });
 });
